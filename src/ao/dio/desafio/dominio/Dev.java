14f368f829670000248e20aa830091d46a473221
Hélder Costa <helderdacsta@hotmail.com>
2023-05-11 16:21:45 +0100

    Criação das classes BootCamp e Dev usando Polimorfismo.

---
+++ src/ao/dio/desafio/dominio/Dev.java
@@ -0,0 +1,75 @@
+package ao.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    
+    private String nome;
+    //Polimorfismo
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+
+    //metodos
+    public void inscreverBootcamp(Bootecamp bootecamp){}
+    public void progredir(){}
+    public void calcularTotalXp(){}
+
+    //gets e sets
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+    public Set<Conteudo> getConteudoConcluidos() {
+        return conteudoConcluidos;
+    }
+    public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudoConcluidos = conteudoConcluidos;
+    }
+//Equal e hashcode
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudosInscritos == null) ? 0 : conteudosInscritos.hashCode());
+        result = prime * result + ((conteudoConcluidos == null) ? 0 : conteudoConcluidos.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudosInscritos == null) {
+            if (other.conteudosInscritos != null)
+                return false;
+        } else if (!conteudosInscritos.equals(other.conteudosInscritos))
+            return false;
+        if (conteudoConcluidos == null) {
+            if (other.conteudoConcluidos != null)
+                return false;
+        } else if (!conteudoConcluidos.equals(other.conteudoConcluidos))
+            return false;
+        return true;
+    }
+
+    
+}
